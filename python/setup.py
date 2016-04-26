baba16fe1a609e68a4dc2e0d62601d32eddfedb3
Valeri Atamaniouk <valeri@swift-nav.com>
2016-04-26 15:53:50 +0300

    GLONASS: added Python bindings for GLONASS message decoder
    Added Python bindings for GLONASS message decoder operations.

--- python/setup.py
+++ python/setup.py
@@ -2,21 +2,22 @@
 from version import get_git_version
 
 setup_args = dict(
-  name = 'swiftnav',
-  version = get_git_version(),
-  description = 'Python bindings to the libswiftnav library.',
-  license = 'LGPLv3',
-  url = 'http://www.swiftnav.com',
-  author = 'Swift Navigation Inc.',
-  author_email = 'dev@swiftnav.com',
-  maintainer = 'Swift Navigation',
-  maintainer_email = 'dev@swiftnav.com',
-  packages = ['swiftnav'],
+    name='swiftnav',
+    version=get_git_version(),
+    description='Python bindings to the libswiftnav library.',
+    license='LGPLv3',
+    url='http://www.swiftnav.com',
+    author='Swift Navigation Inc.',
+    author_email='dev@swiftnav.com',
+    maintainer='Swift Navigation',
+    maintainer_email='dev@swiftnav.com',
+    packages=['swiftnav'],
 )
 
 if __name__ == "__main__":
   import numpy as np
-  import os, sys
+  import os
+  import sys
   from setuptools import setup, Extension
   try:
     from Cython.Distutils import build_ext
@@ -24,6 +25,7 @@ if __name__ == "__main__":
     print "You don't seem to have Cython installed."
     sys.exit(1)
   os.environ['ARCHFLAGS'] = ""
+
   def make_extension(ext_name):
     ext_path = ext_name.replace('.', os.path.sep) + '.pyx'
     library_dirs = []
@@ -32,40 +34,41 @@ if __name__ == "__main__":
     if 'LD_LIBRARY_PATH' in os.environ:
       library_dirs.append(os.environ['LD_LIBRARY_PATH'])
     return Extension(
-      ext_name, [ext_path],
-      include_dirs = [np.get_include(), '.', '../include/'],
-      extra_compile_args = ['-O0', '-g'],
-      extra_link_args = ['-g'],
-      libraries = ['m', 'swiftnav'],
-      library_dirs = library_dirs,
+        ext_name, [ext_path],
+        include_dirs=[np.get_include(), '.', '../include/'],
+        extra_compile_args=['-O0', '-g'],
+        extra_link_args=['-g'],
+        libraries=['m', 'swiftnav'],
+        library_dirs=library_dirs,
     )
   ext_names = [
-    'swiftnav.edc',
-    'swiftnav.signal',
-    'swiftnav.coord_system',
-    'swiftnav.constants',
-    'swiftnav.nav_msg',
-    'swiftnav.pvt',
-    'swiftnav.correlate',
-    'swiftnav.track',
-    'swiftnav.almanac',
-    'swiftnav.lambda_',
-    'swiftnav.ephemeris',
-    'swiftnav.linear_algebra',
-    'swiftnav.amb_kf',
-    'swiftnav.time',
-    'swiftnav.observation',
-    'swiftnav.dgnss_management',
-    'swiftnav.ambiguity_test',
-    'swiftnav.baseline',
-    'swiftnav.bits',
-    'swiftnav.filter_utils',
-    'swiftnav.memory_pool',
-    'swiftnav.prns',
-    'swiftnav.sats_management',
-    'swiftnav.tropo',
-    'swiftnav.set',
-    'swiftnav.bit_sync',
+      'swiftnav.edc',
+      'swiftnav.signal',
+      'swiftnav.coord_system',
+      'swiftnav.constants',
+      'swiftnav.nav_msg',
+      'swiftnav.nav_msg_glo',
+      'swiftnav.pvt',
+      'swiftnav.correlate',
+      'swiftnav.track',
+      'swiftnav.almanac',
+      'swiftnav.lambda_',
+      'swiftnav.ephemeris',
+      'swiftnav.linear_algebra',
+      'swiftnav.amb_kf',
+      'swiftnav.time',
+      'swiftnav.observation',
+      'swiftnav.dgnss_management',
+      'swiftnav.ambiguity_test',
+      'swiftnav.baseline',
+      'swiftnav.bits',
+      'swiftnav.filter_utils',
+      'swiftnav.memory_pool',
+      'swiftnav.prns',
+      'swiftnav.sats_management',
+      'swiftnav.tropo',
+      'swiftnav.set',
+      'swiftnav.bit_sync',
   ]
   extensions = [make_extension(name) for name in ext_names]
   setup_args['ext_modules'] = extensions
